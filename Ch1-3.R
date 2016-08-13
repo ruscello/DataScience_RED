f9aef067f077e1adc612088db6c1156ff416db47
Takegawa <ruscello1023.kt@gmail.com>
2016-08-13 11:46:12 +0900

    Delete Ch1-3.R

--- Ch1-3.R
+++
@@ -1,9 +0,0 @@
-body.data[,2]                      # 列番号を指定して取得
-body.data[,c(1,3)]                 # 複数の列番号を指定して取得
-body.data[,"weight"]               # 列名で取得
-body.data$weight                   # $列名でも取れる
-body.data[, c("height","weight")]  # 複数の列名で取得
-
-body.data[body.data$gender=="F",]                  # 条件にあった行だけ取り出す
-body.data[order(body.data$height),]                 # 昇順でソート
-body.data[order(body.data$height,decreasing = T),]  # 降順でソート
