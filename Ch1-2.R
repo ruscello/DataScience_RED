8991c1280789284f6e2a47d5737ec692ab6243ab
Takegawa <ruscello1023.kt@gmail.com>
2016-08-13 11:46:04 +0900

    Delete Ch1-2.R

--- Ch1-2.R
+++
@@ -1,7 +0,0 @@
-# タブ区切りのデータの読み込み
-body.data <- read.table("body_sample.tsv",header = T,stringsAsFactors = F)
-head(body.data)
-
-# CSVファイルの読み込み
-body.data <- read.csv("body_sample.csv",header = T,stringsAsFactors = F)
-head(body.data)
